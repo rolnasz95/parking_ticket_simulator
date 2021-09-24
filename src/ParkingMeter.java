5b5157acdc907ffcbb920d6d61488b406ba84654
rolnasz95 <kurnasz.roland@gmail.com>
2021-09-24 19:50:10 +0200

    Add ParkingMeter class

---
+++ src/ParkingMeter.java
@@ -0,0 +1,12 @@
+public class ParkingMeter {
+
+    int parkingTime;
+
+    public ParkingMeter(int parkingTime) {
+        this.parkingTime = parkingTime;
+    }
+
+    public int getParkingTime() {
+        return parkingTime;
+    }
+}
